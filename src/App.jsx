aa0a6ffefb6b960874fdc85ce90a479482b4473a
vasanthsakthivel <s.vasanth@live.com>
2024-09-26 13:54:57 +0530

    adding basic cards with add and minus button

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,26 @@
-import React, { useState } from "react";
-import products from "./components/products";
-import Product from "./components/product";
+import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import Cart from './components/cart'
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: < Home />,
+    },
+    {
+        path: "/cart",
+        element: < Cart />
+    }
 
-function App() {
-    const [cart, setCart] = useState([]);
+])
 
-    const handleAddToCart = (id) => {
-        setCart([...cart, id]);
-    };
-
-    const handleRemoveFromCart = (id) => {
-        setCart(cart.filter((itemId) => itemId !== id));
-    };
-
-    const isInCart = (id) => {
-        return cart.includes(id);
-    };
+const App = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container px-4 px-lg-5">
-                    <a className="navbar-brand" href="#!">Start Bootstrap</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                            <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#!">All Products</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#!">Popular Items</a></li>
-                                    <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <button className="btn btn-outline-dark" type="submit">
-                                <i className="bi-cart-fill me-1"></i>
-                                Cart
-                                <span className="badge bg-dark text-white ms-1 rounded-pill">{cart.length}</span>
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-            <header className="bg-dark py-5">
-                <div className="container px-4 px-lg-5 my-5">
-                    <div className="text-center text-white">
-                        <h1 className="display-4 fw-bolder">Shop in style</h1>
-                        <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
-                    </div>
-                </div>
-            </header>
-            <div className="container" >
-                <div className="row">
-                    {products.map((product) => (
-                        <div key={product.id} className="col-md-3">
-                            <Product
-                                product={product}
-                                onAddToCart={handleAddToCart}
-                                onRemoveFromCart={handleRemoveFromCart}
-                                isInCart={isInCart(product.id)}
-                            />
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <footer class="py-5 bg-dark">
-                <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
-            </footer>
+            < RouterProvider router={router} />
         </div>
     )
 }
 
-
-export default App;
+export default App
 
---
+++ src/components/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      cart page displayed is visible
+
+    </div>
+  )
+}
+
+export default Cart;
